0aa2f26791525b1e3783414ad952f58581918a8f
Frances Maxwell <fmaxwell@gmail.com>
2016-09-20 15:06:28 +0100

    Refactored code. It is so beautiful.

--- directory.rb
+++ directory.rb
@@ -1,8 +1,9 @@
+@students = []
+
 def input_students
   puts "Please enter the names of the students. After each name we'll ask for some more detail."
   puts "To finish, just hit return twice"
   # create an empty array
-  students = []
   # get the first name
   # Could use gets.gsub(/\n/," ") instead of chomp
   name = gets.chomp
@@ -28,114 +29,117 @@ def input_students
     end
 
     #add the student hash to the array
-    students << {
+    @students << {
       name: name,
       cohort: cohort.to_sym,
       hobbies: hobbies.to_sym,
       country_of_birth: country_of_birth.to_sym,
     }
 
-    if students.count == 1
-      puts "Now we have #{students.count} student"
+    if @students.count == 1
+      puts "Now we have #{@students.count} student."
     else
-      puts "Now we have #{students.count} students"
+      puts "Now we have #{@students.count} students."
     end
     # get another name from the user
+    puts
+    puts "Please type in another name, or press return to go back to the menu."
     name = gets.chomp
   end
-  # return the array of students
-  students
+end
+
+def print_menu
+  puts
+  puts "-------------------------------------------------------"
+  puts "Welcome to the Student Directory. Please make a choice."
+  puts "-------------------------------------------------------"
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit the program"
+  puts
+end
+
+def show_students
+  if @students.count > 0
+    print_header
+    print_by_cohort
+    print_footer
+  else
+    puts "You haven't entered any students."
+    puts
+  end
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  else
+    puts "I'm sorry, I don't know what you meant. Please try again."
+  end
 end
 
 def interactive_menu
-  students = []
   loop do
-    # 1. Show the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    # 2. Read the input and save it into a variable
-    selection = gets.chomp
-    # 3. Do what the user has asked
-    case selection
-    when "1"
-      students = input_students
-    when "2"
-      print_header(students)
-      print_by_cohort(students)
-      print_footer(students)
-    when "9"
-      exit
-    else
-      puts "I don't know what you meant. Try again."
-    end
+    print_menu
+    process(gets.chomp)
   end
 end
 
+def print_header
+  puts "The students of Villains Academy"
+  puts "--------------------------------"
+end
 
-def print_header students
-  if students.length >= 1
-    puts "The students of Villains Academy"
-    puts "--------------------------------"
+def print
+  count = 0
+  until count == @students.length
+    counter = "#{count + 1}."
+    name = "#{@students[count][:name]}"
+    cohort = "#{@students[count][:cohort]} cohort"
+    hobby = "#{@students[count][:hobbies]}"
+    country = "#{@students[count][:country_of_birth]}"
+    puts counter.center(5) + name.center(15) + cohort.center(15) + hobby.center(15) + country.center(15)
+    count += 1
   end
 end
 
-def print(students)
-
-  if students.length >= 1
-    count = 0
-    until count == students.length
-      counter = "#{count + 1}."
-      name = "#{students[count][:name]}"
-      cohort = "#{students[count][:cohort]} cohort"
-      hobby = "#{students[count][:hobbies]}"
-      country = "#{students[count][:country_of_birth]}"
-      puts counter.center(5) + name.center(15) + cohort.center(15) + hobby.center(15) + country.center(15)
-      count += 1
-    end
-  else
-    puts "You haven't entered any students."
+def print_by_cohort
+  puts
+  # 1. Find all the cohorts in the array
+  cohorts_list = []
+  @students.each do |student|
+    cohorts_list << student[:cohort].to_s
   end
-end
-
-def print_by_cohort(students)
-  if students.length >= 1
-    # 1. Find all the cohorts in the array
-    cohorts_list = []
-    students.each do |student|
-      cohorts_list << student[:cohort].to_s
-    end
-    # 2. Remove duplicates
-    cohorts_list = cohorts_list.uniq
-    # 3. For each cohort, list the students in it
-    cohorts_list.each do |cohort|
-      puts "Students in the #{cohort} cohort: "
-      puts
-      puts "Name".center(20) + "Hobbies".center(20) + "Country".center(20)
-      puts "-----------------------------------------------------------------"
-      students.each do |student|
-        if student[:cohort].to_s == cohort
-          name = "#{student[:name]}"
-          hobby = "#{student[:hobbies]}"
-          country = "#{student[:country_of_birth]}"
-
-          puts name.center(20) + hobby.center(20) + country.center(20)
-        end
+  # 2. Remove duplicates
+  cohorts_list = cohorts_list.uniq
+  # 3. For each cohort, list the students in it
+  cohorts_list.each do |cohort|
+    puts "Students in the #{cohort} cohort: "
+    puts
+    puts "Name".center(20) + "Hobbies".center(20) + "Country".center(20)
+    puts "-----------------------------------------------------------------"
+    @students.each do |student|
+      if student[:cohort].to_s == cohort
+        name = "#{student[:name]}"
+        hobby = "#{student[:hobbies]}"
+        country = "#{student[:country_of_birth]}"
+        puts name.center(20) + hobby.center(20) + country.center(20)
       end
-      puts
     end
-  else
-    puts "You haven't entered any students."
+    puts
   end
 end
 
-def print_footer(students)
-  if students.length >= 1
-    if students.count == 1
-      puts "Overall, we have #{students.count} great student"
-    elsif students.count > 1
-      puts "Overall, we have #{students.count} great students"
-    end
+def print_footer
+  if @students.count == 1
+    puts "Overall, we have #{@students.count} great student"
+  elsif @students.count > 1
+    puts "Overall, we have #{@students.count} great students"
   end
 end
 
